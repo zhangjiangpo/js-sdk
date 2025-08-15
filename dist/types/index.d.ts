adcfdab8946e08cac1b9aea31d8eab1b514bd8a2
zhangjiangpo <zhangjiangpo@aixuexi.com>
2025-08-15 11:02:37 +0800

    feat: 调整顺讯

--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { App } from 'vue';
-export declare const install: (app: App) => App<any>;
-declare const _default: {
-    version: string;
-    install: (app: App<any>) => App<any>;
-};
-export default _default;
